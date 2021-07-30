eb125ae1435970b8b0cd0d9421e97fe646135d8e
Yusuf Enes Aras <y.enesaras@hotmail.com>
2021-07-30 14:21:25 +0300

    refactor

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-    public interface ICarDal:IEntityRepository<Car>
+    interface IEntityRepository
     {
-
     }
 }
